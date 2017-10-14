d240aa8165b2b52fdba1c6871b8841cd7f665ef1
Nate Hascup <nshascup@gmail.com>
2017-10-14 00:54:29 -0700

    pushed to wrong repository

--- app/src/main/java/com/example/nshas/guessinggame/LandingActivity.java
+++ app/src/main/java/com/example/nshas/guessinggame/LandingActivity.java
@@ -1,16 +1,30 @@
 package com.example.nshas.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LandingActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
-
+        Button startGame = (Button) findViewById(R.id.startGame);
+        startGame.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(LandingActivity.this, GuessingActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/nshas/guessinggame/ResultsActivity.java
+++ app/src/main/java/com/example/nshas/guessinggame/ResultsActivity.java
@@ -1,13 +1,34 @@
 package com.example.nshas.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    TextView results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+
+
+
+
+        Button reStartGame = (Button) findViewById(R.id.reStartGame);
+        reStartGame.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ResultsActivity.this, GuessingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
